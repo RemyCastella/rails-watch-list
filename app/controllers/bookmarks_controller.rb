f6d724dfc210585d708ba92a89034b6eb6ee4a14
Remy Castella <remy.jpen@gmail.com>
2025-02-13 13:34:20 +0900

    implement basic user stories

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(list_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(list_params[:movie_id])
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
